14540cfeba99a3c7b37dc2c45d96c449d56c38b6
Steve Brewer <steve.brewer@futurelearn.com>
2017-08-05 17:30:18 +0100

    Add random meal generator

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -6,4 +6,8 @@ class MealsController < ApplicationController
   def show
     @meal = Meal.find(params[:id])
   end
+
+  def random_meal
+    @random_meal = Meal.order("RANDOM()").first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   resources :meals
+  get '/random-meal', to: 'meals#random_meal', as: 'random_meal'
 end
