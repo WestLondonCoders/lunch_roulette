6ea8722d8491d2a0639f5d810e27da92e6c1a789
Steve Brewer <steve.brewer@futurelearn.com>
2017-08-05 17:43:05 +0100

    Add ability to delete meals

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,13 +1,23 @@
 class MealsController < ApplicationController
+  before_action :find_meal, only: [:show, :destroy]
+
   def index
     @meals = Meal.all
   end
 
-  def show
-    @meal = Meal.find(params[:id])
-  end
-
   def random_meal
     @random_meal = Meal.order("RANDOM()").first
+  end
+
+  def destroy
+    if @meal.destroy
+      redirect_to meals_path
+    end
+  end
+
+  private
+
+  def find_meal
+    @meal = Meal.find(params[:id])
   end
 end
